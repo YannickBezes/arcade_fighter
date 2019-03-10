c989b8831053bb3566c87f44916bb6d404f3cabe
moftian <m_fennira@yahoo.com>
2019-03-10 18:15:31 +0100

    menu squelette

--- arcade_fighter/Assets/Scripts/MainMenu.cs
+++ arcade_fighter/Assets/Scripts/MainMenu.cs
@@ -14,4 +14,15 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void OnButtonHoverEnter(GameObject btn)
+    {
+        RectTransform transform = btn.GetComponent<RectTransform>();
+        transform.localScale = transform.localScale * 0.95f;
+    }
+
+    public void OnButtonHoverExit(GameObject btn)
+    {
+        btn.GetComponent<RectTransform>().transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+    }
 }

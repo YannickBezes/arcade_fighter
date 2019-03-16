7c8d4ab5389bdeeac97c569e0d07d6423d20582e
Florian CHEVALIER <gassenbube@gmail.com>
2019-03-16 16:07:31 +0100

    Modification de la fin du jeu
    Mode BO5 actuellement (Ajout d'un système de score)
    Possibilité de faire la manche suivante avec un bonus pour le perdant (125% hp, non cumulable)

---
+++ arcade_fighter/Scripts/DecorScript.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DecorScript : MonoBehaviour
+{
+    public float gravity;
+
+    public Camera cam;
+    public float camMinX, camMaxX, camMinY, camMaxY;
+    private GameObject[] charactersList;
+    private GameObject player1, player2;
+    public int player1Selection, player2Selection;
+
+    // Items stuff
+    public GameObject[] items;
+    public int itemSpawnRate;
+    public float minSpawnPos, maxSpawnPos, spawnHeight;
+    public bool canSpawnItem = true;
+
+    private bool showPauseMenu = false;
+    private GameObject pauseMenu;
+    public KeyCode pause;
+    private GameObject endMenu;
+
+    public GameObject scoreP1, scoreP2;
+
+    // Start is called before the first frame update
+    void Start() {
+        pauseMenu = GameObject.Find("PauseMenu");
+        endMenu = GameObject.Find("EndMenu");
+        endMenu.SetActive(false);
+
+        DataScript.NumberOfGamesToWin = 3;
+        scoreP1.GetComponent<Text>().text = DataScript.ScorePlayer1.ToString();
+        scoreP2.GetComponent<Text>().text = DataScript.ScorePlayer2.ToString();
+
+        Transform t = GameObject.Find("CharactersList").transform;
+
+        charactersList = new GameObject[t.childCount];
+        for (int i = 0; i < t.childCount; i++)
+            charactersList[i] = t.GetChild(i).gameObject;
+
+        foreach (GameObject go in charactersList)
+            go.SetActive(false);
+
+        player1 = charactersList[player1Selection];
+        player2 = charactersList[player2Selection];
+
+        foreach (GameObject p in new GameObject[] {player1, player2}) {
+            p.SetActive(true);
+            p.GetComponent<Rigidbody2D>().gravityScale = gravity;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        MoveCamera();
+
+        if(Random.Range(0, 1000) < itemSpawnRate && canSpawnItem) {
+            Vector3 pos = new Vector3(Random.Range(minSpawnPos, maxSpawnPos), spawnHeight, 0);
+            Instantiate(items[(int)Random.Range(0, items.Length)], pos, transform.rotation);
+            canSpawnItem = false;
+        }
+
+        if(Input.GetKeyDown(pause)) {
+            showPauseMenu = !showPauseMenu;
+        }
+
+        if (showPauseMenu) {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        } else {
+            HidePauseMenu();
+        }
+
+
+		if(player1.GetComponent<Player>() != null && player2.GetComponent<Player>() != null) {
+			if (player1.GetComponent<Player>().hp <= 0 || player2.GetComponent<Player>().hp <= 0) {
+				Time.timeScale = 0;
+				if (player1.GetComponent<Player>().hp <= 0) {
+                    endMenu.SetActive(true);
+                    if (GameObject.Find("Player1Wins") != null) {
+                        DataScript.ScorePlayer2++;
+                        DataScript.BuffPlayer1 = 1.25f;   //Multiplie la vie du joueur 1 par 1.25
+                        GameObject.Find("Player1Wins").SetActive(false);
+                        if (DataScript.ScorePlayer2 >= DataScript.NumberOfGamesToWin)
+                        {
+                            GameObject.Find("ButtonNextMatch").SetActive(false);
+                            GameObject.Find("ButtonNextMatchBuff").SetActive(false);
+                        }
+					}
+				} else {
+                    endMenu.SetActive(true);
+                    if (GameObject.Find("Player2Wins") != null) {
+                        DataScript.ScorePlayer1++;
+                        DataScript.BuffPlayer2 = 1.25f;   //Multiplie la vie du joueur 2 par 1.25
+                        GameObject.Find("Player2Wins").SetActive(false);
+                        if (DataScript.ScorePlayer1 >= DataScript.NumberOfGamesToWin)
+                        {
+                            GameObject.Find("ButtonNextMatch").SetActive(false);
+                            GameObject.Find("ButtonNextMatchBuff").SetActive(false);
+                        }
+                    }
+				}
+			}
+		}
+        
+    }
+
+    public void HidePauseMenu() {
+        showPauseMenu = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MoveCamera() {
+        Vector3 midpoint = (player1.transform.position + player2.transform.position) / 2f;
+        if(midpoint.x < camMinX) {
+            cam.transform.position = new Vector3(camMinX, cam.transform.position.y, cam.transform.position.z);
+        } else if (midpoint.x > camMaxX) {
+            cam.transform.position = new Vector3(camMaxX, cam.transform.position.y, cam.transform.position.z);
+        } else {
+            cam.transform.position = new Vector3(midpoint.x, cam.transform.position.y, cam.transform.position.z);
+        }
+    }
+
+    public GameObject GetPlayer1()
+    {
+        return player1;
+    }
+
+    public GameObject GetPlayer2()
+    {
+        return player2;
+    }
+}
